11456a745a0a2a322a5c9db8ad10dfef9e58a6b3
Leah Post <leah@splitkb.com>
2023-10-25 17:40:17 +0200

    Add Vial support for splitkb.com's Elora

---
+++ keyboards/splitkb/elora/keymaps/vial/config.h
@@ -0,0 +1,33 @@
+/* Copyright 2023 splitkb.com <support@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// Myriad boilerplate
+#define MYRIAD_ENABLE
+
+// Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed
+#define DEBOUNCE 5
+
+/// Vial-specific settings
+
+// Default is 2, needed because keymap has 7 layers
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define VIAL_KEYBOARD_UID {0xB3, 0x8D, 0x94, 0xDA, 0xB3, 0xD7, 0xDC, 0x3D}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 5 }
